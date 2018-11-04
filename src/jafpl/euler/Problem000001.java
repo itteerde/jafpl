29278c7a0e5608542aa388db35443d6fba4a8f18
Erik Itter <erik.itter@gmail.com>
2018-11-04 19:17:49 +0100

    changed Problem000002 to current checking system

--- src/jafpl/euler/Problem000001.java
+++ src/jafpl/euler/Problem000001.java
@@ -4,15 +4,7 @@ public class Problem000001 {
 
 	public static void main(String[] args) {
 
-		int sum = 0;
-
-		for (int n = 0; n < 1000; n++) {
-			if (n % 3 == 0 || n % 5 == 0) {
-				sum += n;
-			}
-		}
-
-		System.out.println(sum);
+		System.out.println(solve());
 
 	}
 
