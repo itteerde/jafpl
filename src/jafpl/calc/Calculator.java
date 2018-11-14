4a8f973d3854f493a02c954bade231cd8adfa6b8
Erik Itter <erik.itter@gmail.com>
2018-11-14 04:39:39 +0100

    ...

--- src/jafpl/calc/Calculator.java
+++ src/jafpl/calc/Calculator.java
@@ -5,10 +5,14 @@ package jafpl.calc;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.Serializable;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 /**
@@ -17,11 +21,25 @@ import javax.swing.JTextField;
  */
 public class Calculator extends JFrame implements ActionListener {
 
+	/**
+	 * @see Serializable
+	 */
 	private static final long serialVersionUID = 6514956977138255134L;
 	private static final String APPLICATION_TITLE = "Simple Java Calculator";
 
 	JFrame mainFrame;
 	JTextField display;
+	JPanel digitPanel;
+	JButton buttonDigit0;
+	JButton buttonDigit1;
+	JButton buttonDigit2;
+	JButton buttonDigit3;
+	JButton buttonDigit4;
+	JButton buttonDigit5;
+	JButton buttonDigit6;
+	JButton buttonDigit7;
+	JButton buttonDigit8;
+	JButton buttonDigit9;
 
 	/**
 	 * @param args
@@ -44,13 +62,29 @@ public class Calculator extends JFrame implements ActionListener {
 		mainFrame.setSize(500, 400);
 		mainFrame.setLocationRelativeTo(null);
 
-		display = new JTextField("0");
+		display = new JTextField("Test Text, 0");
 		display.setEditable(false);
 		display.setHorizontalAlignment(JTextField.RIGHT);
+		display.setFont(new Font("Arial", Font.PLAIN, 20));
 
 		mainFrame.add(display, BorderLayout.NORTH);
+		initDigitPanel();
+		mainFrame.add(digitPanel);
 
 		mainFrame.setVisible(true);
+	}
+
+	/**
+	 * 
+	 */
+	private void initDigitPanel() {
+		digitPanel = new JPanel();
+		buttonDigit0 = new JButton("0");
+		digitPanel.add(buttonDigit0);
+		buttonDigit1 = new JButton("1");
+		digitPanel.add(buttonDigit1);
+		buttonDigit2 = new JButton("2");
+		digitPanel.add(buttonDigit2);
 	}
 
 	/*
