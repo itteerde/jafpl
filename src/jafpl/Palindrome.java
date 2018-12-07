ba65dea1329af61a74083780c000eb6c9f5d36aa
itteerde <erik.itter@gmail.com>
2018-12-07 14:18:53 +0100

    ...

---
+++ src/jafpl/Palindrome.java
@@ -0,0 +1,26 @@
+package jafpl;
+
+public class Palindrome {
+
+	private static String PALINDROM_1 = "neben";
+	private static String PALINDROM_2 = "Otto";
+	private static String PALINDROM_3 = "Reliefpfeiler";
+	private static String NOT_PALINDROM_1 = "ifjsdjfios";
+
+	public static void main(String[] args) {
+
+		System.out.println("\"" + PALINDROM_1 + "\" ist Palindrom?: " + isPalindrom(PALINDROM_1));
+	}
+
+	/**
+	 * Tests if a word provided is a palindrome.
+	 * 
+	 * @param word Word to be tested.
+	 * @return <code>true</code> if <code>word</code> is palindrom,
+	 *         <code>false</code> otherwise.
+	 */
+	private static boolean isPalindrom(String word) {
+		return false;
+	}
+
+}
